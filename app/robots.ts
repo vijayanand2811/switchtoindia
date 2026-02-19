09b84d032848df2cfdb7ae3f1fcac512ff390c37
vijayanand2811 <vijayanand2811@hotmail.com>
2026-02-19 23:33:18 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://switchtoindia.in/sitemap.xml',
+    sitemap: 'https://switchtoindia.vercel.app/sitemap.xml',
   }
 }
